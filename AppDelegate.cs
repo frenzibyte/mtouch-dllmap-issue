e87f82b3ef31adecb565616eed94d2eabc44c4cf
Salman Ahmed <frenzibyte@gmail.com>
2021-11-14 15:46:42 +0300

    Add simple project and solution

---
+++ AppDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace MtouchDllmapIssue
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        public override UIWindow Window { get; set; }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            // create a new window instance based on the screen size
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+            Window.RootViewController = new UIViewController();
+
+            Window.BackgroundColor = UIColor.SystemGreenColor;
+
+            // make the window visible
+            Window.MakeKeyAndVisible();
+            
+            return true;
+        }
+    }
+}
+
+
